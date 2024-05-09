6d4051f6f6488fa1decd83b97e763edad127e132
Kirill Astrahancev <picnichek1996@yandex.ru>
2024-05-09 20:32:35 +0500

    updated Memory models

--- places_remember_project/places/models.py
+++ places_remember_project/places/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import CustomUser
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='profile_photos', blank=True)
+User = CustomUser()
 
 
 class Memory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    location = models.CharField(max_length=200)
-    comment = models.TextField()
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    location = models.CharField(
+        max_length=200, blank=True, null=True, verbose_name='Местоположение')
+    comment = models.TextField(
+        blank=True, null=True, verbose_name='Комментарий')
     latitude = models.FloatField()
-    longitute = models.FloatField()
+    longitude = models.FloatField()
