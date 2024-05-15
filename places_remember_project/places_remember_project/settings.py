601bde14dad380bacd3fc36b5b72070b1504a806
Kirill Astrahancev <picnichek1996@yandex.ru>
2024-05-16 00:01:15 +0500

    updated settings, set postgres db

--- places_remember_project/places_remember_project/settings.py
+++ places_remember_project/places_remember_project/settings.py
@@ -96,9 +96,17 @@ WSGI_APPLICATION = 'places_remember_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
